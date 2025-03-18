0562a8b6a503d4ffa3f7908b17ad3a62012aae5c
Gabriel <diasbusiness18@gmail.com>
2025-03-17 21:24:50 -0300

    atualizando parte de história e adicionando notícias

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,6 +6,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Feedback from './Feedback';
 import React, { useEffect, useState } from "react";
+import tecnologia from '../imgs/tecnologia.jpeg'
+import pratico from '../imgs/pratico.jpeg'
+import alistamentofem from '../imgs/alistamentofem.jpg'
 
 
 
@@ -22,8 +25,9 @@ const Feedbacks = ({category = "general", country = "br", limit = 10}) => {
   };
   
   const news = [
-{img: '', title: "", description:''}
-
+{img: tecnologia, title: 'Exército e Governo de São Paulo firmam parceiria em Ciência, Tecnologia e Inovação', description:' O Exército Brasileiro, por intermédio do Departamento de Ciência e Tecnologia (DCT), e o Governo do Estado de São Paulo firmaram, no dia 12 de março, um protocolo de intenções com o objetivo de fortalecer e expandir as parcerias nas áreas de Ciência, Tecnologia e Inovação. A iniciativa visa a criação de um ambiente colaborativo para o desenvolvimento de projetos que envolvem tanto o setor público quanto o setor militar, alinhando os interesses institucionais e as políticas nacionais de defesa.'},
+{img: pratico, title:'Brigada conduz Exercício Prático de Mobilidade e Vias de Acesso no RJ', description: 'O Grupamento de Unidades Escola da 9ª Brigada de Infantaria Motorizada (GUEs/9ª Bda Inf Mtz), por meio do 57º Batalhão de Infantaria Motorizado (Escola), realizou no Campo de Instrução de Gericinó (CIG), no Rio de Janeiro/RJ, uma importante demonstração de corredores de mobilidade e vias de acesso para os alunos do Curso de Aperfeiçoamento de Oficiais (CAO) da Escola de Aperfeiçoamento de Oficiais (EsAO).'},
+{img: alistamentofem, title:'Mais de 26 mil mulheres se alistaram para o Serviço Militar Inicial Feminino', description:'O alistamento para o Serviço Militar Inicial Feminino é algo inédito no Brasil. Apesar de não ser obrigatório, ele tem atraído grande procura da população feminina. Desde o dia 1º de janeiro, mais de 26 mil mulheres se alistaram para o processo de seleção, demonstrando o interesse em servir ao País e incorporar-se às Forças Armadas. O período para o alistamento, tanto para homens como para mulheres, segue até 30 de junho deste ano, com 1.500 vagas abertas a mulheres que completam 18 anos em 2025 e almejam incorporar como soldados.'}
   ]
   
 
@@ -38,7 +42,7 @@ const Feedbacks = ({category = "general", country = "br", limit = 10}) => {
           {news.map((data,index) => (
             <div key={index}>
               <Feedback 
-              img = {data.urlToImage}
+              img = {data.img}
               title = {data.title}
               description = {data.description}
             />
--- src/components/QuemSou.jsx
+++ src/components/QuemSou.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './quemSou.module.css';
-import hoami from '../imgs/prec.jpeg';
+import hoami from '../imgs/comunicante.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLandmark } from '@fortawesome/free-solid-svg-icons';
 
@@ -14,35 +14,9 @@ const quemSou = () => {
               <h2 className={styles.subtitle}>
               <FontAwesomeIcon icon={faLandmark} /> História
               </h2>
-              <h1 className={styles.mayara}>História dos Precursores</h1>
-              <p className={styles.description}>
-              Desde a Segunda Guerra Mundial, as operações
-              aeroterrestres têm moldado o campo de batalha,
-              impulsionando o Brasil a modernizar suas Forças
-              Armadas. Nesse contexto, surgiram os precursores
-              paraquedistas, militares especializados em
-              operações de envolvimento vertical, que se
-              tornaram uma força estratégica essencial para o
-              Exército Brasileiro (EB).
-              </p>
-              <p className={styles.description}>Assim, ao longo de décadas, os precursores
-              paraquedistas desempenharam papel crucial em
-              diversos momentos da história militar brasileira. Por
-              exemplo, a criação do primeiro Curso de Operações
-              Especiais do EB, o desenvolvimento da doutrina de
-              busca e salvamento, que deu origem ao Esquadrão
-              Aéreo de Salvamento da Força Aérea Brasileira, a
-              abertura de diversas pistas de pouso na imensidão
-              amazônica e o início das instruções especiais para
-              os Cadetes da Academia Militar das Agulhas
-              Negras (AMAN).</p>
-              <p className={styles.description}>A história dos precursores remonta à década de
-              1940, quando jovens oficiais e sargentos do
-              Exército Brasileiro foram enviados a outros países
-              para realizar cursos de especialização em
-              paraquedismo militar, entre eles o Pathfinder. Essa
-              iniciativa pioneira culminou na criação da Escola de
-              Paraquedistas, em 1945.</p>
+              <h1 className={styles.mayara}>Comunicações na Segunda Guerra</h1>
+              <p className={styles.description}>Durante a Segunda Guerra Mundial, as comunicações do Exército Brasileiro desempenharam um papel crucial nas operações da Força Expedicionária Brasileira (FEB) na Itália. A utilização de rádios, cabos telefônicos e mensageiros foi essencial para coordenar ataques, movimentações de tropas e apoio logístico em um terreno desafiador. Os operadores de rádio, muitas vezes em posições avançadas, enfrentavam dificuldades como interferências, sabotagem inimiga e condições climáticas adversas. O treinamento especializado em criptografia e transmissões seguras garantiu que as mensagens não fossem interceptadas, contribuindo para o sucesso das operações da FEB, incluindo a célebre vitória na Batalha de Monte Castello.</p>
+             
             </div>
             <div className="col-md-6 d-flex align-items-center justify-content-center">
               <img
